89c0f6a2ed2b4591acf3d5e331b3a47485ab6842
davidmtz01 <95955080+davidmtz01@users.noreply.github.com>
2024-06-14 01:35:18 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,18 +4,15 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import {URL,DB_HOST,DB_DATABASE,DB_PORT} from './config.js'
 import rutasPerro from './Routes/Perro.routes.js'
-import path from 'path';
 //import rutasAuth from './Routes/Auth.routes.js'
 //const conexion = 'mongodb://'+DB_HOST+':'+DB_PORT+'/'+DB_DATABASE
 mongoose.connect(URL).then()
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(rutasPerro);  // Renombrado para trabajar con perros
 
 app.use( (req,res) => {
